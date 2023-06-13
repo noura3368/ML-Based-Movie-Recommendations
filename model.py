100ab6eb738cee0b67a9ea5445c9449c6d38f4eb
Noura <noura3368@gmail.com>
2023-06-12 22:22:18 -0400

    Adding movie to df and calculating cosine similarity

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ import requests
 def get_string_from_df(df, col):
     df_list = df[col].tolist()
     v1 = " ".join(map(str, df_list))
+    #print(v1)
     return v1
 
 def send_api_requests(url, create_dataframe=False, key=''):
@@ -27,22 +28,6 @@ def send_api_requests(url, create_dataframe=False, key=''):
 # loading the data from csv file to pandas df
 movies = pd.read_csv('files/movies_metadata.csv', low_memory=False)
 
-selected_features = ['genres', 'keywords', 'tagline', 'director', 'cast']
-
-for feature in selected_features:
-    movies[feature] = movies[feature].fillna('')
-
-# combining all the 5 selected features 
-combined_features = movies['genres'] + ' ' + movies['keywords'] + ' ' + movies['tagline'] + ' ' + movies['director'] + ' ' + movies['cast']
-
-# converting text to feature vector/converting to numerical value to cosine value
-vectorizer = TfidfVectorizer()
-
-features = vectorizer.fit_transform(combined_features) # creating a numerical val of combined features
-
-# find similarity score using cosine similarity
-similarity = cosine_similarity(features)
-
 movie_name = input("Enter favourite movie name: ").lower()
 
 csv_movie_name = list(map(str.lower, list(movies['title'])))
@@ -54,7 +39,6 @@ if movie_name in csv_movie_name:
 else:
     url = "https://api.themoviedb.org/3/search/movie?query=" + quote(movie_name)+ "&page=1"
     found_movie = send_api_requests(url=url)['results'][0]
-    
     # get movie deta
     url = "https://api.themoviedb.org/3/movie/"+ str(found_movie['id'])
     movie_details = send_api_requests(url=url, create_dataframe=True, key='genres')
@@ -63,7 +47,7 @@ else:
     
     # get movie keywords
     url += "/keywords"
-    keywords = get_string_from_df(df=movie_details[0], col='name')
+    keywords = get_string_from_df(df=send_api_requests(url=url, create_dataframe=True, key='keywords')[0], col='name')
 
     # get the cast from api 
     cast_string = ""
@@ -74,8 +58,25 @@ else:
             cast_string += c['original_name'] + " "
         elif 'job' in c.keys() and c['job'] == 'director':
             director += c['name'] + " "
+    new_row = pd.DataFrame([{"genres": genres, "keywords": keywords, "tagline": tagline, "title": found_movie['original_title'], "cast": cast_string[:-1], "director": director[:-1]}])
     api_parsed_features = genres + ' ' + keywords + ' ' + tagline + ' ' + director[:-1] + ' ' + cast_string[:-1]
+    movies = pd.concat([new_row, movies.loc[:]]).reset_index(drop=True)
+    index_of_movie = 0
 
+selected_features = ['genres', 'keywords', 'tagline', 'director', 'cast']
+
+for feature in selected_features:
+    movies[feature] = movies[feature].fillna('')
+
+# combining all the 5 selected features 
+combined_features = movies['genres'] + ' ' + movies['keywords'] + ' ' + movies['tagline'] + ' ' + movies['director'] + ' ' + movies['cast']
+# converting text to feature vector/converting to numerical value to cosine value
+vectorizer = TfidfVectorizer()
+
+features = vectorizer.fit_transform(combined_features) # creating a numerical val of combined features
+
+# find similarity score using cosine similarity
+similarity = cosine_similarity(features)
 # getting list of similar movies, first val index of movie, second is the similarity score of movie vs inputted movie
 similarity_score = sorted(list(enumerate(similarity[index_of_movie])), key = lambda x:x[1], reverse=True)
 
