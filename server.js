1ffc6d0db1d38b1954b37b9a137eb6d5414a10fb
Noura <noura3368@gmail.com>
2023-07-31 21:29:41 -0400

    Fixing server, ways to get POST/GET server

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
-import express from "express"
-import cors from "cors"
-import recs from "./api/recs.route.js"
+// initialize project with npm init 
+// database code 
+// env variables for your created password
+//import app from "./server.js"
+//import mongodb from "mongodb"
+//import recsDAO from "./dao/recsDAO.js" // common pattern to write objects that works with db
 
-const app = express()
-app.use(cors()) // middle ware
+import express from "express"; // calls express package 
+import http from "http";
+import fs from "fs";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+var app = express()
 app.use(express.json())
 
-app.use("/api/v1/recs", recs)
-app.use("*", (req, res) => 
-res.status(404).json({error: "not found"}))
+app.get('/', function(request, response) {
+    console.log('GET /')
+    response.sendFile(__dirname + "/index.html")
+    //response.writeHead(200, {'Content-Type':'text/html'})
+    //response.end(__dirname + "/index.html")
+});
+app.get('/movies', function(request, response) {
+    console.log('GET /')
+    response.sendFile(__dirname + "/movies.html")
+});
+app.post('/movies', (req, res) => {
+    console.log("noura!")
+    console.log(req["body"]["val"])
+    res.sendFile(__dirname + "/movies.html")
+});
 
-// export app as module, seperate main server code from db code
-export default app
+const port = 3000
+app.listen(port)
+console.log(`Listening at http://localhost:${port}`)
