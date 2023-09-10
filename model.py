282893da37f9a5779ee20f2195e63a0f133761e3
Noura <noura3368@gmail.com>
2023-09-10 16:50:45 -0400

    Adding post/get functionality, modifying model.py to accept input from post

--- model.py
+++ model.py
@@ -4,13 +4,44 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from urllib.parse import quote
 import requests
+import sys
+from pymongo import MongoClient
+import pymongo
+
+
+def _connect_mongo(host, port, username, password, db):
+    """ A util for making a connection to mongo """
+
+    if username and password:
+        #mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
+        mongo_uri = "mongodb://localhost:27017"
+        conn = MongoClient(mongo_uri)
+    else:
+        conn = MongoClient(host, port)
+    return conn[db]
+
+def read_mongo(db, collection, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
+    """ Read from Mongo and Store into DataFrame """
+
+    # Connect to MongoDB
+    db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
+
+    # Make a query to the specific DB and Collection
+    cursor = db[collection].find(query)
+
+    # Expand the cursor and construct the DataFrame
+    df =  pd.DataFrame(list(cursor))
+    
+    # Delete the _id
+    if no_id:
+        del df['_id']
+
+    return df, db[collection]
 
 # Data collection and pre-processing 
-
 def get_string_from_df(df, col):
     df_list = df[col].tolist()
     v1 = " ".join(map(str, df_list))
-    #print(v1)
     return v1
 
 def send_api_requests(url, create_dataframe=False, key=''):
@@ -25,61 +56,65 @@ def send_api_requests(url, create_dataframe=False, key=''):
         return [df, response]
     return response
 
+def return_lower_case_title(title):
+    return str(title).lower()
+
 # loading the data from csv file to pandas df
-movies = pd.read_csv('files/movies_metadata.csv', low_memory=False)
+def main(movie_name=""):
+    movies, collection_value = read_mongo(db="ListofMediaInfo", collection="Movies", username="noura3368", password="YRq4o0IXwfYgQXQK")
+    csv_movie_name = []
+    for title in movies['title']:
+        csv_movie_name.append(return_lower_case_title(title))
+    # finding a recommendation based off of movie given from user
+    if movie_name in csv_movie_name:
+        index_of_movie = csv_movie_name.index(movie_name)
 
-movie_name = input("Enter favourite movie name: ").lower()
+    else:
+        url = "https://api.themoviedb.org/3/search/movie?query=" + quote(movie_name)+ "&page=1"
+        found_movie = send_api_requests(url=url)['results'][0]
+        # get movie data
+        url = "https://api.themoviedb.org/3/movie/"+ str(found_movie['id'])
+        movie_details = send_api_requests(url=url, create_dataframe=True, key='genres')
+        genres = get_string_from_df(df=movie_details[0], col='name')
+        
+        # get movie keywords
+        url += "/keywords"
+        keywords = get_string_from_df(df=send_api_requests(url=url, create_dataframe=True, key='keywords')[0], col='name')
 
-csv_movie_name = list(map(str.lower, list(movies['title'])))
+        dictionary_of_new_row_movie = {"genres": genres, "keywords": keywords, "title": found_movie['original_title']}
+        if str(found_movie['original_title']).lower() not in csv_movie_name:
+            new_row_from_user = pd.DataFrame([dictionary_of_new_row_movie])
+            api_parsed_features = genres + ' ' + keywords 
+            movies = pd.concat([new_row_from_user, movies.loc[:]]).reset_index(drop=True)
+            index_of_movie = 0
+            collection_value.insert_one(dictionary_of_new_row_movie)
+        else:
+            index_of_movie = csv_movie_name.index(str(found_movie['original_title'].lower()))
 
-# finding a recommendation based off of movie given from user
-if movie_name in csv_movie_name:
-    index_of_movie = csv_movie_name.index(movie_name)
+    selected_features = ['genres', 'keywords']
 
-else:
-    url = "https://api.themoviedb.org/3/search/movie?query=" + quote(movie_name)+ "&page=1"
-    found_movie = send_api_requests(url=url)['results'][0]
-    # get movie deta
-    url = "https://api.themoviedb.org/3/movie/"+ str(found_movie['id'])
-    movie_details = send_api_requests(url=url, create_dataframe=True, key='genres')
-    genres = get_string_from_df(df=movie_details[0], col='name')
-    tagline = movie_details[1]['tagline']
-    
-    # get movie keywords
-    url += "/keywords"
-    keywords = get_string_from_df(df=send_api_requests(url=url, create_dataframe=True, key='keywords')[0], col='name')
+    for feature in selected_features:
+        movies[feature] = movies[feature].fillna('')
 
-    # get the cast from api 
-    cast_string = ""
-    director = ""
-    cast = send_api_requests(url=url.replace('keywords', 'credits'))['cast']
-    for c in cast:
-        if 'character' in c.keys() and c['character'] != '':
-            cast_string += c['original_name'] + " "
-        elif 'job' in c.keys() and c['job'] == 'director':
-            director += c['name'] + " "
-    new_row = pd.DataFrame([{"genres": genres, "keywords": keywords, "tagline": tagline, "title": found_movie['original_title'], "cast": cast_string[:-1], "director": director[:-1]}])
-    api_parsed_features = genres + ' ' + keywords + ' ' + tagline + ' ' + director[:-1] + ' ' + cast_string[:-1]
-    movies = pd.concat([new_row, movies.loc[:]]).reset_index(drop=True)
-    index_of_movie = 0
+    # combining all the 5 selected features 
+    combined_features = movies['genres'] + ' ' + movies['keywords'] 
+    # converting text to feature vector/converting to numerical value to cosine value
+    vectorizer = TfidfVectorizer()
 
-selected_features = ['genres', 'keywords', 'tagline', 'director', 'cast']
+    features = vectorizer.fit_transform(combined_features) # creating a numerical val of combined features
 
-for feature in selected_features:
-    movies[feature] = movies[feature].fillna('')
+    # find similarity score using cosine similarity
+    similarity = cosine_similarity(features)
+    # getting list of similar movies, first val index of movie, second is the similarity score of movie vs inputted movie
+    similarity_score = sorted(list(enumerate(similarity[index_of_movie])), key = lambda x:x[1], reverse=True)
 
-# combining all the 5 selected features 
-combined_features = movies['genres'] + ' ' + movies['keywords'] + ' ' + movies['tagline'] + ' ' + movies['director'] + ' ' + movies['cast']
-# converting text to feature vector/converting to numerical value to cosine value
-vectorizer = TfidfVectorizer()
-
-features = vectorizer.fit_transform(combined_features) # creating a numerical val of combined features
-
-# find similarity score using cosine similarity
-similarity = cosine_similarity(features)
-# getting list of similar movies, first val index of movie, second is the similarity score of movie vs inputted movie
-similarity_score = sorted(list(enumerate(similarity[index_of_movie])), key = lambda x:x[1], reverse=True)
-
-for index in range(0, 30):
-    print(str(index + 1) + ". " + csv_movie_name[similarity_score[index + 1][0]])
-
+    movie_rec_dict = {}
+    for index in range(0, 20):
+        url = "https://api.themoviedb.org/3/search/movie?query=" + quote(csv_movie_name[similarity_score[index + 1][0]])+ "&page=1"
+        found_movie = send_api_requests(url=url)['results']
+        if len(found_movie) > 0:
+            found_movie = found_movie[0]["poster_path"]
+            movie_rec_dict[csv_movie_name[similarity_score[index + 1][0]]] = "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + found_movie
+    print(movie_rec_dict)   
+    #sys.stdout.flush()
+main(movie_name=return_lower_case_title(sys.argv[1]))
--- server.js
+++ server.js
@@ -1,35 +1,49 @@
 // initialize project with npm init 
 // database code 
 // env variables for your created password
-//import app from "./server.js"
-//import mongodb from "mongodb"
-//import recsDAO from "./dao/recsDAO.js" // common pattern to write objects that works with db
-
 import express from "express"; // calls express package 
 import http from "http";
 import fs from "fs";
+import {spawn} from "child_process"
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 var app = express()
+app.use(express.urlencoded()); 
 app.use(express.json())
+//app.use(bodyParser.json());
+
+// set the view engine to ejs
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    console.log('GET /')
-    response.sendFile(__dirname + "/index.html")
+    //response.sendFile(__dirname + "/index.html")
+    response.render(__dirname + "/index" 
+    )
     //response.writeHead(200, {'Content-Type':'text/html'})
     //response.end(__dirname + "/index.html")
 });
-app.get('/movies', function(request, response) {
-    console.log('GET /')
-    response.sendFile(__dirname + "/movies.html")
+/*
+app.get('/movies', function(request, response, ...availabeChoices) {
+    console.log("HELLO!", request)
+    response.render(__dirname + "/views/movies")
 });
-app.post('/movies', (req, res) => {
-    console.log("noura!")
-    console.log(req["body"]["val"])
-    res.sendFile(__dirname + "/movies.html")
+*/
+
+app.post('/', (req, res) => {
+    const s = spawn("python", ['model.py', req.body.movie]) 
+    s.stdout.on('data', (data) => { 
+        var movies = data.toString()
+        res.send({'movies':movies})
+        //res.send({"movies":movies, "url":"/movies"});
+        //res.render("movies", {movies:movies})
+        //res.redirect(__dirname + '/views/movies.ejs', {movies:movies})
+    });
+    //res.sendFile(__dirname + "/views/movies.ejs")
 });
 
 const port = 3000
