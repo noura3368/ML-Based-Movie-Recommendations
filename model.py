04d4d882bf7b805dab85746d55ce910108ac8946
Noura <noura3368@gmail.com>
2024-01-10 00:15:14 -0500

    Removing extra api call to fetch poster path

--- model.py
+++ model.py
@@ -81,7 +81,7 @@ def main(movie_name=""):
         url += "/keywords"
         keywords = get_string_from_df(df=send_api_requests(url=url, create_dataframe=True, key='keywords')[0], col='name')
 
-        dictionary_of_new_row_movie = {"genres": genres, "keywords": keywords, "title": found_movie['original_title']}
+        dictionary_of_new_row_movie = {"genres": genres, "keywords": keywords, "title": found_movie['original_title'], "poster_path":found_movie["poster_path"]}
         if str(found_movie['original_title']).lower() not in csv_movie_name:
             new_row_from_user = pd.DataFrame([dictionary_of_new_row_movie])
             api_parsed_features = genres + ' ' + keywords 
@@ -89,7 +89,9 @@ def main(movie_name=""):
             index_of_movie = 0
             collection_value.insert_one(dictionary_of_new_row_movie)
         else:
+            print(csv_movie_name, "\n")
             index_of_movie = csv_movie_name.index(str(found_movie['original_title'].lower()))
+        #collection_value.insert_one({"genres": genres, "keywords":keywords, "original_title":movie_name, "title":movie_name})
 
     selected_features = ['genres', 'keywords']
 
@@ -110,11 +112,11 @@ def main(movie_name=""):
 
     movie_rec_dict = {}
     for index in range(0, 21):  
-        url = "https://api.themoviedb.org/3/search/movie?query=" + quote(csv_movie_name[similarity_score[index + 1][0]])+ "&page=1"
-        found_movie = send_api_requests(url=url)['results']
+        #url = "https://api.themoviedb.org/3/search/movie?query=" + quote(csv_movie_name[similarity_score[index + 1][0]])+ "&page=1"
+        #found_movie = send_api_requests(url=url)['results']
         if len(found_movie) > 0:
-            found_movie = found_movie[0]["poster_path"]
-            movie_rec_dict[csv_movie_name[similarity_score[index + 1][0]]] = "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + str(found_movie)
+            #found_movie = found_movie[0]["poster_path"]
+            movie_rec_dict[csv_movie_name[similarity_score[index + 1][0]]] = "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + found_movie['poster_path']
     print(movie_rec_dict)
     #sys.stdout.flush()
 
